7b89797935186529cf547ed252ba41f3640f9ce8
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-05-09 16:46:12 +0200

    [JQ-123] dfgs

--- begcm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-ticket=$(echo "$(git branch --show-current)" | awk -F'-' '{print "["$1"-"$2"]"}')
-sha=$(git log --oneline | fzf --reverse --header='Select a commit' | awk '{print $1}')
-
-git filter-branch -f --msg-filter "s/^/$ticket/" "$sha"..HEAD
