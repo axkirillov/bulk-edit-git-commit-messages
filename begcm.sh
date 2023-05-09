d758ddbbe4cc3fac4beb1918f1fb29cdaab6bc21
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-05-09 16:36:08 +0200

    [JQ-123] sadkfjhadg

--- begcm.sh
+++ begcm.sh
@@ -4,4 +4,4 @@
 ticket=$(echo "$(git branch --show-current)" | awk -F'-' '{print "["$1"-"$2"]"}')
 sha=$(git log --oneline | fzf --reverse --header='Select a commit' | awk '{print $1}')
 
-git filter-branch -f --msg-filter "echo \"$ticket \$(cat)\"" "$sha"..HEAD
+git filter-branch -f --msg-filter "s/^/$ticket/" "$sha"..HEAD
