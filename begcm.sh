ff0f89681682e9246d936490224dc1c2455c49f1
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-05-22 14:46:10 +0200

    use git filter repo

--- begcm.sh
+++ begcm.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+git commit -a -m "you forgot to commit this"
 
 ticket=$(echo "$(git branch --show-current)" | awk -F'-' '{print "["$1"-"$2"]"}')
 sha=$(git log --oneline | fzf --reverse --header='Select a commit' | awk '{print $1}')
 
-git filter-branch -f --msg-filter "echo \"$ticket \$(cat)\"" "$sha"..HEAD
+git filter-repo -f --message-callback "return b\"$ticket \" + message" --refs "$sha"..HEAD
